87eb9fa2e222dca886db0c679fedc3046d048768
Shanu <128120971+Shancom900@users.noreply.github.com>
2023-11-12 12:31:21 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -5,11 +5,11 @@ from pyrogram import Client, Filters
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Updates Channel", url="https://t.me/SDBOTs_inifinity")
+        [InlineKeyboardButton("Updates Channel", url="https://t.me/SHONU_TOOLS")
       ],
       [ 
         InlineKeyboardButton(
-            "Support Group", url="https://t.me/SDBOTz")]
+            "Support Group", url="https://t.me/SHONU")]
     ])  
-    helptxt = f"<b> Just send a Youtube url to download it in video or audio format!\n\n ~ @Damantha_Jasinghe </b>"
+    helptxt = f"<b> Just send a Youtube url to download it in video or audio format!\n\n ~ @SHONU </b>"
     await message.reply_text(helptxt, reply_markup=joinButton)
