ea34f981e8e6bc8d3145cf69afc6c91f5433f650
Shanu <128120971+Shancom900@users.noreply.github.com>
2023-11-26 18:52:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import  os
 
-BOT_TOKEN = os.environ.get("5926529146:AAHRobcD8OVfJdcTRWgefTXzPZ0YGjyniKc")
+BOT_TOKEN = os.environ.get("5853334562:AAHHAPeIXAC2s6rjQlPHQrYKoaExkWLHT7U")
 APP_ID = int(os.environ.get("27359621"))
 API_HASH = os.environ.get("6202d9add6fce5b6f8b71f731842d7bc")
 
